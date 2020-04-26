dc8d303b1f9d184a37bbfc2ea63266eebaaf0da0
Anatolii Stepaniuk <astepaniuk@tektelic.com>
2020-04-26 21:19:12 +0300

    Few more methods

--- sheet_utils.py
+++ sheet_utils.py
@@ -1,6 +1,6 @@
 class SheetMeta:
-    def __init__(self, sheet_client, document_id, page_name):
-        self.sheet_client = sheet_client
+    def __init__(self, client, document_id, page_name):
+        self.sheet_client = client
         self.document_id = document_id
         self.page_name = page_name
 
@@ -10,3 +10,42 @@ def write_cell_value(sheet_meta, column, row, value):
     cell_full = sheet_meta.page_name + "!" + cell + ":" + cell
     body = {'values': [[value]]}
     sheet_meta.sheet_client.values().update(spreadsheetId=sheet_meta.document_id, range=cell_full, valueInputOption='RAW', body=body).execute()
+
+
+def create_spread_sheet(client, spreadsheet_title, sheet_title):
+    body = {
+        'properties': {'title': spreadsheet_title},
+        'sheets': [
+            {
+                'properties': {'title': sheet_title}
+            }
+        ]
+    }
+
+    return client.create(body=body).execute()
+
+
+def make_first_row_bold(client, spreadsheet_id, sheet_id):
+    body = {
+        "requests": [
+            {
+                "repeatCell": {
+                    "cell": {
+                        "userEnteredFormat": {
+                            "textFormat": {
+                                "bold": True
+                            }
+                        }
+                    },
+                    "range": {
+                        "endRowIndex": 1,
+                        "sheetId": sheet_id
+                    },
+                    "fields": "userEnteredFormat.textFormat.bold"
+                }
+            }
+        ]
+    }
+
+    client.batchUpdate(spreadsheetId=spreadsheet_id, body=body).execute()
+
